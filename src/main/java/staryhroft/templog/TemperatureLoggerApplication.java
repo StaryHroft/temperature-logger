0d99116b780ac1f912bc3e7bb774a60e2fbf2e0c
Man <os@mail.com>
2026-02-23 10:20:29 +0300

    feat(scheduler): add weather update scheduler

--- src/main/java/staryhroft/templog/TemperatureLoggerApplication.java
+++ src/main/java/staryhroft/templog/TemperatureLoggerApplication.java
@@ -2,11 +2,16 @@ package staryhroft.templog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
+@EnableAsync
 public class TemperatureLoggerApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(TemperatureLoggerApplication.class, args);
     }
 
